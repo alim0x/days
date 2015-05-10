b622d8690dfea1c79ed82ccbcefe462fb2917204
Mo <alim0x@outlook.com>
2015-05-10 16:14:09 +0800

    basic structure in handle address

---
+++ java_ver/src/ChooseImg.java
@@ -0,0 +1,12 @@
+/**
+ * Created by Mo on 2015-05-10.
+ */
+public class ChooseImg {
+    public String todayImg() {
+
+    }
+
+    public String randomImg() {
+
+    }
+}
--- java_ver/src/DownloadImg.java
+++ java_ver/src/DownloadImg.java
@@ -2,7 +2,6 @@
  * Created by Mo on 2015-05-04.
  */
 
-import org.apache.http.client.*;
 import java.util.regex.*;
 
 public class DownloadImg {
